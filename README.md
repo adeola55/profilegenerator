7e9b5a6c51fbaa02a718a5d4aac29aecd89c77fc
adeola afariogun <kokobilo55@gmail.com>
2020-04-23 00:07:21 -0700

    added project title and description to input

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 
     # GITHUB USERNAME: adeola55
-    ### PROJECT1: updated-portfolio
-    Email: shhjc.com
+    ### PROJECT1: kool
+    Email: mkll.mm
     Following: 0
     Followers: 0
     Public repo: 12
+    Project title: 
+    Description: kioo
     
--- index.js
+++ index.js
@@ -5,18 +5,28 @@ var fs = require("fs")
 inquirer.prompt([
     {
         type:"input",
-        messgae:"what is your github username?",
+        message:"what is your github username?",
         name:"github",
     },
     {
         type:"input",
-        messgae:"what is your email address?",
+        message:"what is your email address?",
         name:"email",
     },
     {
         type:"input",
-        messgae:"what is your project URL?",
+        message:"what is your project URL?",
         name:"url",
+    },
+    {
+        type: "input",
+        message:"what is your project title?",
+        name:"project title"
+    },
+    {
+        type: "input",
+        message:"what is your description?",
+        name:"description"
     }
 ])
 .then(function(response){
@@ -32,9 +42,12 @@ inquirer.prompt([
     Following: ${apiresponse.data.following}
     Followers: ${apiresponse.data.followers}
     Public repo: ${[apiresponse.data.public_repos]}
+    Project title: ${[userinput.project_title]}
+    Description: ${[userinput.description]}
     `
     return readmetext
 })
 .then(function(data){
     fs.writeFileSync("./README.md", data)
 })
+
